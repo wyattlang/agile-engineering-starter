558d6ec1ebd2ad5a1e3088ae1801f726343de754
wyattlang <wyattlang2001@gmail.com>
2022-06-21 13:41:24 -0400

    emptied the UserCrudService class

--- inventory-manager/service/db/user-crud-service.js
+++ inventory-manager/service/db/user-crud-service.js
@@ -3,46 +3,16 @@ import { PrismaClient } from '@prisma/client';
 
 import CrudService from './crud-service.js';
 
-const prisma = new PrismaClient();
-
 export default class UserCrudService extends CrudService {
 
-  async save(user) {
-    delete user.id;
-    return prisma.user.create({
-      data: user
-    });
+  constructor() {
+    this.prismaClient = new PrismaClient();
   }
 
-  async findById(id) {
-    return prisma.user.findUnique({
-      where: {
-        id: Number.parseInt(id)
-      }
-    });
-  }
-
-  async findAll() {
-    return prisma.user.findMany();
-  }
-
-  async update(user) {
-    return prisma.user.update({
-      where: {
-        id: Number.parseInt(user.id)
-      },
-      data: {
-        balance: Number.parseFloat(user.balance)
-      }
-    });
-  }
-
-  async delete(id) {
-    return prisma.user.delete({
-      where: {
-        id: Number.parseInt(id)
-      }
-    });
-  }
+  /**
+   *
+   * implement the 5 CrudService methods here
+   *
+   */
 
 }
