fe2b4975fe21400dc4b64412669c5c15d9caed1f
wyattlang <wyattlang2001@gmail.com>
2022-06-21 13:23:27 -0400

    emptied PlantMarketClient for lab3.1 starter

--- inventory-manager/service/http-client/plant-market-client.js
+++ inventory-manager/service/http-client/plant-market-client.js
@@ -5,12 +5,6 @@ import MarketClient from './market-client.js';
 
 export default class PlantMarketClient extends MarketClient {
 
-  async findById(id) {
-    return axios.get(`http://localhost:8084/plant/${id}`).then(response => response.data);
-  }
 
-  async findAll() {
-    return axios.get('http://localhost:8084/plant').then(response => response.data);
-  }
 
 }
